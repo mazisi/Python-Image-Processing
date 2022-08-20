03df903bed469ae721ec19dded47016c939e9620
Msebele Mazisi <mazisimsebele18@gmail.com>
2022-08-20 21:49:13 +0200

    Adding comments

--- JPEGToPNGConverter.py
+++ JPEGToPNGConverter.py
@@ -7,7 +7,9 @@ from PIL import Image
 #check if new folder exists..if not create it...
 if not os.path.exists('new_images'):
     os.makedirs('new_images')
-for file_name in os.listdir('images'):#Loop through images and convert to png
+for file_name in os.listdir('images'):     #Loop through images and convert to png
     img = Image.open('images/'+file_name)
-    clean_file_name = os.path.splitext(file_name)[0]#remove.jpg in filenames
+    clean_file_name = os.path.splitext(file_name)[0]  #remove .jpg extension in filenames
     img.save('new_images/'+clean_file_name+'.png', 'png')
+
+
